222e055383e5b30adda2d2a3e7bfdcc547d341d2
Damiano <34246799+Mek101@users.noreply.github.com>
2019-02-25 09:13:18 +0000

    Format godd*****

--- server/index.php
+++ server/index.php
@@ -1,45 +1,43 @@
 <?php
 // process client request (via URL)
-	header ("Content-Type_application/json");
-	include ("functions.php");
-	if(!empty($_GET['name'])){
-			$name=$_GET['name'];
+	header("Content-Type_application/json");
+	include("functions.php");
+
+	if(isset($_GET['name']) && !empty($_GET['name'])){
+		$name=$_GET['name'];
 			
-			switch($name){
-				case 1:
-					$cont = fumetti();
-					if($cont == 0)
-						deliver_response(404,"not found", NULL);
-					else
-					{
-						deliver_response(200, 'success', $cont);
-					}
-					
-					break;
-				case 2:
-					$books = orderSconto();
-					//$jsonBooks = json_encode($books);
-					if($books == NULL)
-						deliver_response(404,"not found", NULL);
-					else
-						deliver_response(200,"success", $books);
-					
-					break;
-				case 3:
-					
-					$archivio = dataarc($_GET['start'], $_GET['end']);
-					if($archivio == NULL)
-						deliver_response(404,"not found", NULL);
-					else
-						deliver_response(200, "success", $archivio);
-					break;
+		switch($name){
+			case 1:
+				$cont = fumetti();
+				if($cont == 0)
+					deliver_response(404,"not found", NULL);
+				else					
+					deliver_response(200, 'success', $cont);
+				
+				break;
+			case 2:
+				$books = orderSconto();
+				//$jsonBooks = json_encode($books);
+				if($books == NULL)
+					deliver_response(404,"not found", NULL);
+				else
+					deliver_response(200,"success", $books);
+				
+				break;
+			case 3:		
+				$archivio = dataarc($_GET['start'], $_GET['end']);
+				if($archivio == NULL)
+					deliver_response(404,"not found", NULL);
+				else
+					deliver_response(200, "success", $archivio);
+				
+				break;
 			}	
 	}
-	else
-	{
-		//throw invalid request
+	else	
+		// Return invalid request
 		deliver_response(400,"Invalid request", NULL);
-	}
+	
 	
 	function deliver_response($status, $status_message, $data)
 	{
