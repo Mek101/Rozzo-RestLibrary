0627072a1c3eb502769ecaa832bc9a28664bc3d2
Mek101 <oidgmek101@gmail.com>
2019-03-11 08:12:05 +0100

    Written first server querirer method prototype.

--- server/database_querier.php
+++ server/database_querier.php
@@ -1,3 +1,15 @@
 <?php
-
+	class Querier {
+		private $_pdo;
+		
+		public function __construct(string $host, string $dbName, string $username, string passwd) {
+			$pdo = new PDO("mysql:host=$host;dbname=$dbName", $username, $passwd);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		
+		
+		public function getRepart(string £repart) : int {
+		
+		}
+	}
 ?>
